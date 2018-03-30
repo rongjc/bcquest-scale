7ced92b95e68a7302b8abb5ffe55bbc087019927
jc <juanchengrong@gmail.com>
2018-03-30 10:08:08 +0800

    added styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'mobx-react';
+
+import Header from './components/Header';
 import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
+import SideBar from './components/SideBar';
 
 import ContractSetup from './components/ContractSetup';
 import TokenSetup from './components/TokenSetup';
@@ -18,9 +21,13 @@ class App extends Component {
       <Provider {...stores}>
         <Router>
           <div className="App">
-            <Route path="/contract" component={ContractSetup} />
-            <Route path="/token" component={TokenSetup} />
-            <Route path="/crowdsale" component={CrowdsaleSetup} />
+            <Header />
+            <SideBar />
+            <div className="content-wrapper">
+              <Route path="/contract" component={ContractSetup} />
+              <Route path="/token" component={TokenSetup} />
+              <Route path="/crowdsale" component={CrowdsaleSetup} />
+            </div>
           </div>
         </Router>
       </Provider>
